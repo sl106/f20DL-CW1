7a669db01f8ed07279124ce80052f8d2cd24ca12
sl106 <36174763+sl106@users.noreply.github.com>
2020-11-09 10:57:12 +0000

    Update task7_sl106.py

--- task7_sl106.py
+++ task7_sl106.py
@@ -8,7 +8,8 @@ X = pd.read_csv("DataSets/new200.csv")
 y = pd.read_csv("resource/y_train_smpl.csv")
 X.insert(139, "class", y, allow_duplicates=False)
 
-
+#change values of class attributes to string
 X["class"] = X["class"].map({0: "s0", 1: "s1", 2: "s2", 3: "s3", 4: "s4", 5: "s5", 6: "s6", 7: "s7", 8: "s8", 9: "s9"})
 print(X.head())
+#export to new csv file
 X.to_csv(r'resource/new200-c.csv', index = False, header=True)
